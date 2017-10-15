9aba758be1ae81766141151f7b4425fdb245a572
Клим Зелинский <Alive@home-sweet-home.local>
2017-10-15 22:15:02 +0300

    Initial Commit

---
+++ iOS-App-Lesson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-App-Lesson1
+//
+//  Created by Клим Зелинский on 15.10.17.
+//  Copyright © 2017 Клим Зелинский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
