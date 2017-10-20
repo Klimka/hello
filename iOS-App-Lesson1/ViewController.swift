02c744f35cf8536ae199982835f682ff1c1c459c
Klimka <klimka13@ya.ru>
2017-10-20 19:56:28 +0300

    scroll is finally solved

--- iOS-App-Lesson1/ViewController.swift
+++ iOS-App-Lesson1/ViewController.swift
@@ -25,5 +25,68 @@ class ViewController: UIViewController {
    print("кнопка нажалась!")
     }
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    
+
+
+} // end of class
+
+extension ViewController {
+        // когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+        
+        //получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0)
+        
+        // добавляем отступ внизу UIScrollView равный размеру клавиатуры
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    // когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        // устанавливаем отступ внизу UIScrollView равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func hideKeyboard() {
+        scrollView.endEditing(true)
+    }
+
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //подписываемся на уведомления
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+        //второе когда пропадает
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        
+        }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    
+    }
+    
 }
+
+
+
+
+
 
